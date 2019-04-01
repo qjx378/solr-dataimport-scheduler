0236b450762650160c6713a636abc2704acbf4c8
qjx378 <qjx378@gmail.com>
2019-04-01 12:43:48 +0800

    update solr-core

--- src/main/java/org/apache/solr/handler/dataimport/scheduler/HttpPostScheduler.java
+++ src/main/java/org/apache/solr/handler/dataimport/scheduler/HttpPostScheduler.java
@@ -1,18 +1,16 @@
 /**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 package org.apache.solr.handler.dataimport.scheduler;
 
@@ -25,7 +23,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -35,6 +32,7 @@ import org.slf4j.LoggerFactory;
  * Gets DIH params and sets default values, where appropriate.<br>
  * Uses DIH params to assemble complete URL.<br>
  * Invokes URL using HTTP POST request.<br>
+ * 
  * @author mbonaci
  */
 public class HttpPostScheduler extends TimerTask {
@@ -52,13 +50,14 @@ public class HttpPostScheduler extends TimerTask {
     private static final Logger logger = LoggerFactory.getLogger(HttpPostScheduler.class);
 
     public HttpPostScheduler(String webAppName, Timer t) throws Exception {
-        
+
         // load properties from global dataimport.properties
         p = new SolrDataImportProperties();
         reloadParams();
         fixParams(webAppName);
 
-        if (!syncEnabled.equals("1")) throw new Exception("Schedule disabled");
+        if (!"1".equals(syncEnabled))
+            throw new Exception("Schedule disabled");
 
         if (syncCores == null || (syncCores.length == 1 && syncCores[0].isEmpty())) {
             singleCore = true;
@@ -72,20 +71,24 @@ public class HttpPostScheduler extends TimerTask {
     private void reloadParams() {
         p.loadProperties(true);
         syncEnabled = p.getProperty(SolrDataImportProperties.SYNC_ENABLED);
-        cores       = p.getProperty(SolrDataImportProperties.SYNC_CORES);
-        server      = p.getProperty(SolrDataImportProperties.SERVER);
-        port        = p.getProperty(SolrDataImportProperties.PORT);
-        webapp      = p.getProperty(SolrDataImportProperties.WEBAPP);
-        params      = p.getProperty(SolrDataImportProperties.PARAMS);
-        interval    = p.getProperty(SolrDataImportProperties.INTERVAL);
-        syncCores   = cores != null ? cores.split(",") : null;
+        cores = p.getProperty(SolrDataImportProperties.SYNC_CORES);
+        server = p.getProperty(SolrDataImportProperties.SERVER);
+        port = p.getProperty(SolrDataImportProperties.PORT);
+        webapp = p.getProperty(SolrDataImportProperties.WEBAPP);
+        params = p.getProperty(SolrDataImportProperties.PARAMS);
+        interval = p.getProperty(SolrDataImportProperties.INTERVAL);
+        syncCores = cores != null ? cores.split(",") : null;
     }
 
     private void fixParams(String webAppName) {
-        if (server == null   || server.isEmpty())     server = "localhost";
-        if (port == null     || port.isEmpty())       port = "8080";
-        if (webapp == null   || webapp.isEmpty())     webapp = webAppName;
-        if (interval == null || interval.isEmpty() || getIntervalInt() <= 0) interval = "30";
+        if (server == null || server.isEmpty())
+            server = "localhost";
+        if (port == null || port.isEmpty())
+            port = "8080";
+        if (webapp == null || webapp.isEmpty())
+            webapp = webAppName;
+        if (interval == null || interval.isEmpty() || getIntervalInt() <= 0)
+            interval = "30";
     }
 
     public void run() {
@@ -96,11 +99,11 @@ public class HttpPostScheduler extends TimerTask {
                 logger.info("<index update process> Reloading global dataimport.properties");
                 reloadParams();
 
-            // single-core
+                // single-core
             } else if (singleCore) {
                 prepUrlSendHttpPost();
 
-            // multi-core
+                // multi-core
             } else if (syncCores.length == 0 || (syncCores.length == 1 && syncCores[0].isEmpty())) {
                 logger.warn("<index update process> No cores scheduled for data import");
                 logger.info("<index update process> Reloading global dataimport.properties");
@@ -111,7 +114,7 @@ public class HttpPostScheduler extends TimerTask {
                     prepUrlSendHttpPost(core);
                 }
             }
-        } catch(Exception e) {
+        } catch (Exception e) {
             logger.error("Failed to prepare for sendHttpPost", e);
             reloadParams();
         }
@@ -141,11 +144,11 @@ public class HttpPostScheduler extends TimerTask {
         try {
 
             URL url = new URL(completeUrl);
-            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
             conn.setRequestMethod("POST");
             conn.setRequestProperty("type", "submit");
-            conn.setRequestProperty("Content-Type","application/html");
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
             conn.setDoOutput(true);
 
             // Send HTTP POST
@@ -160,7 +163,7 @@ public class HttpPostScheduler extends TimerTask {
             logger.info(core + "<index update process> Response message\t\t\t" + conn.getResponseMessage());
             logger.info(core + "<index update process> Response code\t\t\t" + conn.getResponseCode());
 
-            //listen for change in properties file if an error occurs
+            // listen for change in properties file if an error occurs
             if (conn.getResponseCode() != 200) {
                 reloadParams();
             }
@@ -183,7 +186,7 @@ public class HttpPostScheduler extends TimerTask {
             return Integer.parseInt(interval);
         } catch (NumberFormatException e) {
             logger.warn("Unable to convert 'interval' to number. Using default value (30) instead", e);
-            return 30; //return default in case of error
+            return 30; // return default in case of error
         }
     }
 }
